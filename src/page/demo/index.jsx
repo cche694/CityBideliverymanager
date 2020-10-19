b67eb3889420f79b22340042bb22fd1f66e0b357
chang__ccge <b449568227@gmail.com>
2020-10-19 20:03:38 +0800

    craco less

---
+++ src/page/demo/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Button} from "antd"
+class Demo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div><Button></Button></div>
+         );
+    }
+}
+ 
+export default Demo;
