559f9abeddf85f2734432f96d81358c76042b972
chang__ccge <b449568227@gmail.com>
2020-10-21 17:09:19 +0800

    jsonp init

--- src/jsonp/index.js
+++ src/jsonp/index.js
@@ -1,12 +1,20 @@
-import jsonP from "jsonp"
-export default class JsonP_{
-    static jsonp(options){
-        return new Promise((res,rej)=>{
-            jsonP(options.url,{
-                param:"callback"
-            },function(err,res){
-
-            })
-        })
-    }
+import jsonP from "jsonp";
+export default class JsonP_ {
+  static jsonp(options) {
+    return new Promise((resolve, reject) => {
+      jsonP(
+        options.url,
+        {
+          param: "callback",
+        },
+        function (err, response) {
+          if (response.status == "success") {
+            resolve(response);
+          } else {
+            reject(response.message);
+          }
+        }
+      );
+    });
+  }
 }
